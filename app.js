9c8f5e1cc99056b0861394317889bb70fe7036f6
Kinkaid011 <k.nolen46@gmail.com>
2020-02-12 19:54:05 -0500

    setting up database

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
+const { development } = require("./knexfile");
+
+const knex = require("knex")(development);
+
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello World!'))
+app.get('/', async (req, res) => {
+  const posts = await knex("posts");
+  res.send(posts);
+})
 
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log("Server started!"))
-
